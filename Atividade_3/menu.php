50fbd15212bd2afad0cdf397d229f4df0ba386dc
CleberBrito <cleber_brito@hotmail.com>
2014-01-10 12:43:34 -0200

    Create menu.php

---
+++ Atividade_3/menu.php
@@ -0,0 +1,11 @@
+
+<fieldset>
+<a href="index.php">Lista</a>
+&nbsp;&nbsp;
+<a href="cadastrar_form.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="remover_form.php">Remover</a>
+&nbsp;&nbsp;
+<a href="editar_form.php">Editar</a>
+&nbsp;&nbsp;
+</fieldset>
