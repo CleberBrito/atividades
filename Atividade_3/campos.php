74635e594402492337085eaefa295d9437ba9455
CleberBrito <cleber_brito@hotmail.com>
2014-01-10 12:43:57 -0200

    Create campos.php

---
+++ Atividade_3/campos.php
@@ -0,0 +1,15 @@
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <label>Nome do Produto</label><input type="text" name="nome" /><br/>
+    
+    <label>Produto Novo</label><input type="checkbox" name="aceito" checked="checked" /><br/>
+    
+    <label>Vermelho</label><input type="radio" name="opcao" value="Vermelho"/><br/>
+    <label>Preto</label><input type="radio" name="opcao" value="Branco"/><br/>
+    <label>Roxo</label><input type="radio" name="opcao" value="Rosa"/><br/>
+    <select name="selecionar" >
+        <option>5 Dias</option>
+        <option>10 Dias</option>
+        <option>15 Dias</option>
+    <select/><br/><br/><br/><br/>
+    <textarea rows="10" cols="30" name="texto"></textarea>
+        
